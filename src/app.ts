3a2d9e92b6b74131fb640b45a67bbdcf0135d935
Unknown <did-aleksej@yandex.ru>
2017-10-29 00:25:56 +0300

    add API versioning

--- src/app.ts
+++ src/app.ts
@@ -49,6 +49,15 @@ import errorHandler = require("errorhandler");
 const app = express();
 
 /**
+ * Create API versions
+ */
+const v1 = express.Router();
+
+app.use('/v1', v1);
+
+app.use('/', v1); // Set the default version to latest.
+
+/**
  * Swagger config
  */
 module.exports = app; // for testing
@@ -136,42 +145,42 @@ app.use(cors(corsOptions));
  */
 
 // DEBTS
-app.get('/debts', passport.authenticate('jwt', { session: false }), debtsController.getAllUserDebts);
-app.put('/debts', passport.authenticate('jwt', { session: false }), debtsController.createNewDebt);
+v1.get('/debts', passport.authenticate('jwt', { session: false }), debtsController.getAllUserDebts);
+v1.put('/debts', passport.authenticate('jwt', { session: false }), debtsController.createNewDebt);
 
-app.get('/debts/:id', passport.authenticate('jwt', { session: false }), debtsController.getDebtsById);
+v1.get('/debts/:id', passport.authenticate('jwt', { session: false }), debtsController.getDebtsById);
 
-app.post('/debts/:id/creation', passport.authenticate('jwt', { session: false }), debtsController.acceptCreation);
-app.delete('/debts/:id/creation', passport.authenticate('jwt', { session: false }), debtsController.declineCreation);
+v1.post('/debts/:id/creation', passport.authenticate('jwt', { session: false }), debtsController.acceptCreation);
+v1.delete('/debts/:id/creation', passport.authenticate('jwt', { session: false }), debtsController.declineCreation);
 
-app.put('/debts/:id/delete_request', passport.authenticate('jwt', { session: false }), debtsController.requestDebtsDelete);
-app.delete('/debts/:id/delete_request', passport.authenticate('jwt', { session: false }), debtsController.requestDebtsDeleteAccept);
-app.post('/debts/:id/delete_request', passport.authenticate('jwt', { session: false }), debtsController.requestDebtsDeleteDecline);
+v1.put('/debts/:id/delete_request', passport.authenticate('jwt', { session: false }), debtsController.requestDebtsDelete);
+v1.delete('/debts/:id/delete_request', passport.authenticate('jwt', { session: false }), debtsController.requestDebtsDeleteAccept);
+v1.post('/debts/:id/delete_request', passport.authenticate('jwt', { session: false }), debtsController.requestDebtsDeleteDecline);
 
-app.put('/debts/single', passport.authenticate('jwt', { session: false }), debtsController.createSingleDebt);
-app.delete('/debts/single/:id', passport.authenticate('jwt', { session: false }), debtsController.deleteSingleDebt);
+v1.put('/debts/single', passport.authenticate('jwt', { session: false }), debtsController.createSingleDebt);
+v1.delete('/debts/single/:id', passport.authenticate('jwt', { session: false }), debtsController.deleteSingleDebt);
 
 // MONEY OPERATIONS
-app.put('/operation', passport.authenticate('jwt', { session: false }), operationsController.createOperation);
+v1.put('/operation', passport.authenticate('jwt', { session: false }), operationsController.createOperation);
 
-app.delete('/operation/:id', passport.authenticate('jwt', { session: false }), operationsController.deleteOperation);
+v1.delete('/operation/:id', passport.authenticate('jwt', { session: false }), operationsController.deleteOperation);
 
-app.post('/operation/:id/creation', passport.authenticate('jwt', { session: false }), operationsController.acceptOperation);
-app.delete('/operation/:id/creation', passport.authenticate('jwt', { session: false }), operationsController.declineOperation);
+v1.post('/operation/:id/creation', passport.authenticate('jwt', { session: false }), operationsController.acceptOperation);
+v1.delete('/operation/:id/creation', passport.authenticate('jwt', { session: false }), operationsController.declineOperation);
 
 // USERS
-app.get('/users', passport.authenticate('jwt', { session: false }), usersController.getUsersArrayByName);
-app.patch('/users', passport.authenticate('jwt', { session: false }), usersController.uploadImage, usersController.updateUserData);
+v1.get('/users', passport.authenticate('jwt', { session: false }), usersController.getUsersArrayByName);
+v1.patch('/users', passport.authenticate('jwt', { session: false }), usersController.uploadImage, usersController.updateUserData);
 
 /**
  * AUTH
  */
-app.put('/signup/local', authController.localSignUp);
+v1.put('/signup/local', authController.localSignUp);
 
-app.post('/login/local', authController.localLogin);
-app.get('/login/facebook', authController.facebookLogin);
+v1.post('/login/local', authController.localLogin);
+v1.get('/login/facebook', authController.facebookLogin);
 
-app.get('/login_status', passport.authenticate('jwt', { session: false }), authController.checkLoginStatus);
+v1.get('/login_status', passport.authenticate('jwt', { session: false }), authController.checkLoginStatus);
 
 // Need nodemailer
 // app.post('/reset-password', authController.resetPasswordSendEmail);
