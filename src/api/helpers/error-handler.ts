d101255c85cf2763350bae5ed5e327a2bbcb3a23
Unknown <did-aleksej@yandex.ru>
2017-10-28 13:03:31 +0300

    rollbar fix

--- src/api/helpers/error-handler.ts
+++ src/api/helpers/error-handler.ts
@@ -2,6 +2,6 @@ import {rollbar} from "./rollbar";
 
 export let errorHandler = (req, res, err) => {
         res.statusCode = 400;
-        rollbar.error(JSON.stringify(err, req));
+        rollbar.error(JSON.stringify(err), req);
         return res.json({error: err});
 };
